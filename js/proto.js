4887b83ddda301a68a2670cdd1cab6b0ccd995de
SnoopyKim <nombu71@gmail.com>
2018-07-19 23:28:32 +0900

    Get/Set data in sprite/image and send to next Scene
    It needs more retouch

--- js/proto.js
+++ js/proto.js
@@ -14,13 +14,26 @@ class FirstScene extends Phaser.Scene {
     this.input.manager.enabled = true;
     //create character selection
     this.nav1 = this.add.text(16,16,'Choose your character', { fontsize: '32px', fill: '#000' });
-    let result1 = this.add.text(16,48, '', { fontsize: '32px', fill: '#000' });
 
     this.platforms = this.physics.add.staticGroup();
+    let charList = []
     for (let i=0; i<4; i++) {
       let char = this.platforms.create(160*(i+1),this.platformY,'char'+(i+1)).setScale(0.5).setInteractive().setTint(505050);
       char.setName(i+1);
+      charList.push(char);
     }
+    charList[0].setData({
+      hp: 20, atk: 5, def: 1
+    });
+    charList[1].setData({
+      hp: 10, atk: 5, def: 3
+    });
+    charList[2].setData({
+      hp: 10, atk: 8, def: 1
+    });
+    charList[3].setData({
+      hp: 12, atk: 6, def: 2
+    });
 
     this.input.on('gameobjectover', function(pointer, gameObject) {
       gameObject.clearTint();
@@ -29,8 +42,12 @@ class FirstScene extends Phaser.Scene {
       gameObject.setTint(505050);
     }, this);
     this.input.on('gameobjectdown', function(pointer, gameObject) {
-      console.log(gameObject.name);
-      this.scene.start('second', { char: gameObject.name });
+      let charName = gameObject.name;
+      let charStatus = gameObject.getData(['hp','atk','def']);
+      console.log(charName);
+      console.log(charStatus);
+      //change scene with data(image and status)
+      this.scene.start('second', { char: charName, status: charStatus });
     }, this);
   }
 }
@@ -39,19 +56,21 @@ class SecondScene extends Phaser.Scene {
   constructor() {
     super( { key: 'second' } );
   }
+  //get data from pre scene
   init(data) {
     this.playerID = data.char;
     this.player = {
-      HP: 10,
-      ATK: 5,
-      DEF: 1
+      HP: data.status[0],
+      ATK: data.status[1],
+      DEF: data.status[2]
     }
-    this.playerY = 200;
-    this.platformY = 400;
+    this.playerY = 220;
+    this.platformY = 500;
   }
   preload() {
     //load images
     this.load.image('player','img/character'+this.playerID+'.png');
+    this.load.image('status','img/status.png');
     this.load.image('item1','img/item1.png');
     this.load.image('item2','img/item2.png');
     this.load.image('item3','img/item3.png');
@@ -59,19 +78,38 @@ class SecondScene extends Phaser.Scene {
     this.load.image('select','img/select.png')
   }
   create() {
-    this.nav1 = this.add.text(16,16,'Choose your item', { fontsize: '32px', fill: '#000' });
-    let result1 = this.add.text(16,48, '', { fontsize: '32px', fill: '#000' });
+    this.nav1 = this.add.text(20,20,'Choose your item', { fontsize: '40px', fill: '#000' });
 
-    //show chosen character
-    this.add.image(400, this.playerY, 'player').setDisplaySize(200,200);
+    //show chosen character & recent status
+    this.add.image(150, this.playerY, 'player').setDisplaySize(300,320);
+    this.add.image(550, this.playerY, 'status').setDisplaySize(500,320);
+    let HP = this.add.text(320, 140, [
+      'HP: ' + this.player.HP,
+      'ATK: ' + this.player.ATK,
+      'DEF: ' + this.player.DEF
+    ], { fontsize: '40px', fill: '#000' });
 
     //create item selection
     this.platforms = this.physics.add.staticGroup();
+    let itemList = []
     for (let i=0; i<4; i++) {
       this.platforms.create(100+200*i, this.platformY,'select').setDisplaySize(200,200);
       let item = this.platforms.create(100+200*i,this.platformY,'item'+(i+1)).setDisplaySize(180,180).setInteractive().setTint(505050);
       item.setName(i+1);
+      itemList.push(item);
     }
+    itemList[0].setData({
+      hp: 0, atk: 2, def: 0
+    });
+    itemList[1].setData({
+      hp: 10, atk: 0, def: 1
+    });
+    itemList[2].setData({
+      hp: 5, atk: 2, def: 0
+    });
+    itemList[3].setData({
+      hp: 0, atk: 0, def: 2
+    });
 
     this.input.on('gameobjectover', function(pointer, gameObject) {
       gameObject.clearTint();
@@ -80,8 +118,12 @@ class SecondScene extends Phaser.Scene {
       gameObject.setTint(505050);
     }, this);
     this.input.on('gameobjectdown', function(pointer, gameObject) {
-      console.log(gameObject.name);
-      this.scene.start('third', { char: this.playerID, item: gameObject.name });
+      let itemName  = gameObject.name;
+      let itemStatus = gameObject.getData(['hp','atk','def']);
+      console.log(itemName, itemStatus);
+      console.log(this.player);
+      //change scene with data(image and status)
+      this.scene.start('third', { char: this.playerID, status: this.player, item: itemName, plusStatus: itemStatus });
     }, this);
   }
 }
@@ -90,12 +132,13 @@ class ThirdScene extends Phaser.Scene {
   constructor() {
     super({ key: 'third' });
   }
+  //get data from pre scene
   init(data) {
     this.playerID = data.char;
     this.player = {
-      HP: 10,
-      ATK: 5,
-      DEF: 1
+      HP: data.status['HP']+data.plusStatus[0],
+      ATK: data.status['ATK']+data.plusStatus[1],
+      DEF: data.status['DEF']+data.plusStatus[2]
     }
     this.itemID = data.item;
   }
