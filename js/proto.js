26006023eea30b2ac151d6e3c18d8270e4fbef99
SnoopyKim <nombu71@gmail.com>
2018-07-29 02:00:22 +0900

    Add stage clear & game over UI
    + IntroScene change to mobile

--- js/proto.js
+++ js/proto.js
@@ -1,4 +1,17 @@
-let player;
+let gameWidth = window.innerWidth;
+let gameHeight = window.innerHeight;
+let player = {
+  ID: undefined,
+  HP: 0,
+  ATK: 0,
+  DEF: 0,
+  init: function() {
+    this.ID = undefined;
+    this.HP = 0;
+    this.ATK = 0;
+    this.DEF = 0;
+  }
+};
 let equip = {
   weapon: {
     ID: undefined,
@@ -13,37 +26,73 @@ let equip = {
     HP: 0,
     ATK: 0,
     DEF: 0
+  },
+  status: {
+    ID: undefined,
+    PART: "status",
+    HP: 0,
+    ATK: 0,
+    DEF: 0
+  },
+  init: function() {
+    this.weapon = {
+      ID: undefined,
+      PART: "weapon",
+      HP: 0,
+      ATK: 0,
+      DEF: 0
+    };
+    this.armor = {
+      ID: undefined,
+      PART: "weapon",
+      HP: 0,
+      ATK: 0,
+      DEF: 0
+    };
+    this.status = {
+      ID: undefined,
+      PART: "status",
+      HP: 0,
+      ATK: 0,
+      DEF: 0
+    };
   }
 };
+let level = 1;
+let tier = 1;
+let shp=0;
+let satk=0;
+let sdef=0;
 
 class IntroScene extends Phaser.Scene {
   constructor(config) {
     super({ key: 'intro' });
   }
-  init() {
-
-  }
   preload() {
+    this.load.image('bg','img/bg_intro.png');
     this.load.image('title','img/title.png');
     this.load.image('button','img/button.png');
   }
   create() {
-    this.add.image(400, 200, 'title').setDisplaySize(600,300);
+    this.add.image(gameWidth/2, gameHeight/2, 'bg').setDisplaySize(gameWidth, gameHeight);
+    this.add.image(gameWidth/2, 300, 'title').setDisplaySize(gameWidth*0.8,400);
 
-    let btnStart = this.add.image(300, 400, 'button').setDisplaySize(200,100).setInteractive();
+    let btnStart = this.add.image(gameWidth/2-200 , gameHeight-300, 'button').setDisplaySize(400,200).setInteractive();
     btnStart.name = 'start';
-    this.add.text(240, 385, '게임시작', { fontSize: 30, fontStyle: 'bold'});
-    let btnHow = this.add.image(500, 400, 'button').setDisplaySize(200,100).setInteractive();
+    let txtStart = this.add.text(0, 0, '게임시작', { fontSize: '40px', fontStyle: 'bold'});
+    txtStart.x = btnStart.x - txtStart.width/2;
+    txtStart.y = btnStart.y - txtStart.height/2;
+    let btnHow = this.add.image(gameWidth/2+200, gameHeight-300, 'button').setDisplaySize(400,200).setInteractive();
     btnHow.name = 'how';
-    this.add.text(440, 385, '게임방법', { fontSize: 30, fontStyle: 'bold'});
+    let txtHow = this.add.text(0, 0, '게임방법', { fontSize: 40, fontStyle: 'bold'});
+    txtHow.x = btnHow.x - txtHow.width/2;
+    txtHow.y = btnHow.y - txtHow.height/2;
 
-    this.input.on('gameobjectover', function(pointer, button) {
+    this.input.on('gameobjectdown', function(pointer, button) {
       button.setTint(0xf0f0f0);
     });
-    this.input.on('gameobjectout', function(pointer, button) {
+    this.input.on('gameobjectup', function(pointer, button) {
       button.clearTint();
-    });
-    this.input.on('gameobjectdown', function(pointer, button) {
       switch (button.name) {
         case 'start':
           this.scene.start('first');
@@ -53,15 +102,11 @@ class IntroScene extends Phaser.Scene {
       }
     }, this);
   }
-  update() {
-
-  }
 }
 
 class ChooseCharacterScene extends Phaser.Scene {
   constructor(config) {
     super({ key: 'first' });
-    this.platformY = 400;
   }
   preload() {
     //load images
@@ -69,10 +114,12 @@ class ChooseCharacterScene extends Phaser.Scene {
     this.load.image('char2','img/character2.png');
     this.load.image('char3','img/character3.png');
     this.load.image('char4','img/character4.png');
+    this.load.image('UI','img/status.png');
   }
   create() {
     this.input.manager.enabled = true;
     //create character selection
+    this.add.image(0,0, 'UI').setDisplaySize(gameWidth, 200);
     this.nav1 = this.add.text(16,16,'Choose your character', { fontSize: '32px', fill: '#000' });
 
     this.platforms = this.physics.add.staticGroup();
@@ -141,10 +188,20 @@ class ChooseItemScene extends Phaser.Scene {
     //show chosen character & recent status
     this.add.image(150, this.playerY, 'player').setDisplaySize(300,320);
     this.add.image(550, this.playerY, 'status').setDisplaySize(500,320);
+    if(level%5==0){
+      tier++;
+    }
     let playerStatus = {
+      Now: this.add.text(330, 60, 'level: '+level+' tier : '+tier ,{ fontSize: '20px', fill: '#000' }),
       HP: this.add.text(330, 80, 'HP: '+player.HP, { fontSize: '20px', fill: '#000' }),
       ATK: this.add.text(330, 100, 'ATK: '+player.ATK, { fontSize: '20px', fill: '#000' }),
-      DEF: this.add.text(330, 120, 'DEF: '+player.DEF, { fontSize: '20px', fill: '#000' })
+      DEF: this.add.text(330, 120, 'DEF: '+player.DEF, { fontSize: '20px', fill: '#000' }),
+      WHP: this.add.text(430, 80, 'WHP: '+equip.weapon.HP, { fontSize: '20px', fill: '#000' }),
+      WATK: this.add.text(430, 100, 'WATK: '+equip.weapon.ATK, { fontSize: '20px', fill: '#000' }),
+      WDEF: this.add.text(430, 120, 'WDEF: '+equip.weapon.DEF, { fontSize: '20px', fill: '#000' }),
+      SHP: this.add.text(530, 80, 'SHP: '+shp, { fontSize: '20px', fill: '#000' }),
+      SATK: this.add.text(530, 100, 'SATK: '+satk , { fontSize: '20px', fill: '#000' }),
+      SDEF: this.add.text(530, 120, 'SDEF: '+sdef, { fontSize: '20px', fill: '#000' })
     }
 
     this.add.image(430, 250, 'inventory').setDisplaySize(220,220);
@@ -154,7 +211,7 @@ class ChooseItemScene extends Phaser.Scene {
       imgWeapon = this.add.image(430, 250, 'item'+equip.weapon.ID).setDisplaySize(200,200);
     }
     if (equip.armor.ID != undefined) {
-      imgArmor = this.add.image(430, 250, 'item'+equip.weapon.ID).setDisplaySize(200,200);
+      imgArmor = this.add.image(430, 250, 'item'+equip.armor.ID).setDisplaySize(200,200);
     }
 
     //create item selection
@@ -169,37 +226,78 @@ class ChooseItemScene extends Phaser.Scene {
     }
     // item's status instance randomly
     for(let i=0; i<4; i++){
-      let pHp_i=0;
-      let pAtk_i=0;
-      let pDef_i=0;
-      let key = Math.floor(Math.random()*4)+1;
-      if(key==1){
-        pAtk_i = Math.floor(Math.random()*2)+2;
-      }
-      else if(key==2){
-        pHp_i = Math.floor(Math.random()*3)+9;
-      }
-      else if(key==3){
-        pHp_i = Math.floor(Math.random()*3)+4;
-        pAtk_i = Math.floor(Math.random()*2)+1;
-      }
-      else{
-        pDef_i = Math.floor(Math.random()*2)+2;
+      let HP__item=0;
+      let ATK__item=0;
+      let DEF__item=0;
+      let HP__status=0;
+      let ATK__status=0;
+      let DEF__status=0;
+      let part = Math.floor(Math.random()*4)+1;
+      let part_what;
+      if(level%2==1){
+        if(part==1){
+          ATK__item = Math.floor(Math.random()*2)+2*tier;
+          part_what = "weapon";
+        }
+        else if(part==2){
+          HP__item = Math.floor(Math.random()*3)+9*tier;
+          part_what = "weapon";
+        }
+        else if(part==3){
+          HP__item = Math.floor(Math.random()*3)+4*tier;
+          ATK__item = Math.floor(Math.random()*2)+1*tier;
+          part_what = "weapon";
+        }
+        else{
+          DEF__item = Math.floor(Math.random()*2)+2*tier;
+          part_what = "weapon";
+        }
+
+        itemList[i].setData({
+          hp : HP__item,
+          atk : ATK__item,
+          def : DEF__item,
+          part : part_what
+        });
+
+        let item = this.add.text(50+180*i, 350, [
+          'HP: ' + HP__item,
+          'ATK: ' + ATK__item,
+          'DEF: ' + DEF__item,
+        ], { fontsize: '40px', fill: '#000' });
+
+        console.log('hp :' +HP__item + ' atk : ' + ATK__item + ' def : '+DEF__item);
+      }else{
+        part_what = "status";
+        if(part==1){
+          ATK__status = Math.floor(Math.random()*2)+1*tier;
+        }
+        else if(part==2){
+          HP__status = Math.floor(Math.random()*3)+2*tier;
+        }
+        else if(part==3){
+          HP__status = Math.floor(Math.random()*2)+1*tier;
+          ATK__status = Math.floor(Math.random()*1)+1*tier;
+        }
+        else{
+          DEF__status = Math.floor(Math.random()*2)+1*tier;
+        }
+        console.log('hp :' +HP__status + ' atk : ' + ATK__status + ' def : '+DEF__status);
+        itemList[i].setData({
+          hp : HP__status,
+          atk : ATK__status,
+          def : DEF__status,
+          part : part_what
+        });
+
+        let item = this.add.text(50+180*i, 350, [
+          'HP: ' + HP__status,
+          'ATK: ' + ATK__status,
+          'DEF: ' + DEF__status
+        ], { fontsize: '40px', fill: '#000' });
       }
 
-      itemList[i].setData({
-        hp : pHp_i,
-        atk : pAtk_i,
-        def : pDef_i
-      });
-
-      let item = this.add.text(50+180*i, 350, [
-        'HP: ' + pHp_i,
-        'ATK: ' + pAtk_i,
-        'DEF: ' + pDef_i
-      ], { fontsize: '40px', fill: '#000' });
-    }
-
+    }//set random status end
     this.input.on('gameobjectover', function(pointer, gameObject) {
       //clear item tint
       gameObject.clearTint();
@@ -211,47 +309,69 @@ class ChooseItemScene extends Phaser.Scene {
       let itemDEF = gameObject.getData('def');
 
       //show status change
-      if (itemPART=='weapon') {
-        playerStatus.HP.setText('HP: ' + (player.HP-equip.weapon.HP+itemHP));
-        playerStatus.ATK.setText('ATK: ' + (player.ATK-equip.weapon.ATK+itemATK))
-        playerStatus.DEF.setText('DEF: ' + (player.DEF-equip.weapon.DEF+itemDEF))
+        if (itemPART=='weapon') {
+          playerStatus.HP.setText('HP: ' + (player.HP-equip.weapon.HP+itemHP));
+          playerStatus.ATK.setText('ATK: ' + (player.ATK-equip.weapon.ATK+itemATK))
+          playerStatus.DEF.setText('DEF: ' + (player.DEF-equip.weapon.DEF+itemDEF))
 
-        if (equip.weapon.HP<itemHP) {
-          playerStatus.HP.setTintFill(0x00ff00);
-        } else if (equip.weapon.HP>itemHP) {
-          playerStatus.HP.setTintFill(0xff0000);
-        }
-        if (equip.weapon.ATK<itemATK) {
-          playerStatus.ATK.setTintFill(0x00ff00);
-        } else if (equip.weapon.ATK>itemATK) {
-          playerStatus.ATK.setTintFill(0xff0000);
-        }
-        if (equip.weapon.DEF<itemDEF) {
-          playerStatus.DEF.setTintFill(0x00ff00);
-        } else if (equip.weapon.DEF>itemDEF) {
-          playerStatus.DEF.setTintFill(0xff0000);
-        }
-      } else if (itemPART=='armor') {
-        playerStatus.HP.setText('HP: ' + (player.HP-equip.armor.HP+itemHP));
-        playerStatus.ATK.setText('ATK: ' + (player.ATK-equip.armor.ATK+itemATK))
-        playerStatus.DEF.setText('DEF: ' + (player.DEF-equip.armor.DEF+itemDEF))
+          if (equip.weapon.HP<itemHP) {
+            playerStatus.HP.setTintFill(0x00ff00);
+          } else if (equip.weapon.HP>itemHP) {
+            playerStatus.HP.setTintFill(0xff0000);
+          }
+          if (equip.weapon.ATK<itemATK) {
+            playerStatus.ATK.setTintFill(0x00ff00);
+          } else if (equip.weapon.ATK>itemATK) {
+            playerStatus.ATK.setTintFill(0xff0000);
+          }
+          if (equip.weapon.DEF<itemDEF) {
+            playerStatus.DEF.setTintFill(0x00ff00);
+          } else if (equip.weapon.DEF>itemDEF) {
+            playerStatus.DEF.setTintFill(0xff0000);
+          }
+        } else if (itemPART=='armor') {
+          playerStatus.HP.setText('HP: ' + (player.HP-equip.armor.HP+itemHP));
+          playerStatus.ATK.setText('ATK: ' + (player.ATK-equip.armor.ATK+itemATK))
+          playerStatus.DEF.setText('DEF: ' + (player.DEF-equip.armor.DEF+itemDEF))
 
-        if (equip.armor.HP<itemHP) {
-          playerStatus.HP.setTintFill(0x00ff00);
-        } else if (equip.armor.HP>itemHP) {
-          playerStatus.HP.setTintFill(0xff0000);
+          if (equip.armor.HP<itemHP) {
+            playerStatus.HP.setTintFill(0x00ff00);
+          } else if (equip.armor.HP>itemHP) {
+            playerStatus.HP.setTintFill(0xff0000);
+          }
+          if (equip.armor.ATK<itemATK) {
+            playerStatus.ATK.setTintFill(0x00ff00);
+          } else if (equip.armor.ATK>itemATK) {
+            playerStatus.ATK.setTintFill(0xff0000);
+          }
+          if (equip.armor.DEF<itemDEF) {
+            playerStatus.DEF.setTintFill(0x00ff00);
+          } else if (equip.armor.DEF>itemDEF) {
+            playerStatus.DEF.setTintFill(0xff0000);
+          }
+        }else if (itemPART == 'status'){
+          playerStatus.HP.setText('HP: ' + (player.HP+itemHP));
+          playerStatus.ATK.setText('ATK: ' + (player.ATK+itemATK))
+          playerStatus.DEF.setText('DEF: ' + (player.DEF+itemDEF))
+
+          if (equip.status.HP<itemHP) {
+            playerStatus.HP.setTintFill(0x00ff00);
+          } else if (equip.status.HP>itemHP) {
+            playerStatus.HP.setTintFill(0xff0000);
+          }
+          if (equip.status.ATK<itemATK) {
+            playerStatus.ATK.setTintFill(0x00ff00);
+          } else if (equip.status.ATK>itemATK) {
+            playerStatus.ATK.setTintFill(0xff0000);
+          }
+          if (equip.status.DEF<itemDEF) {
+            playerStatus.DEF.setTintFill(0x00ff00);
+          } else if (equip.status.DEF>itemDEF) {
+            playerStatus.DEF.setTintFill(0xff0000);
+          }
         }
-        if (equip.armor.ATK<itemATK) {
-          playerStatus.ATK.setTintFill(0x00ff00);
-        } else if (equip.armor.ATK>itemATK) {
-          playerStatus.ATK.setTintFill(0xff0000);
-        }
-        if (equip.armor.DEF<itemDEF) {
-          playerStatus.DEF.setTintFill(0x00ff00);
-        } else if (equip.armor.DEF>itemDEF) {
-          playerStatus.DEF.setTintFill(0xff0000);
-        }
-      }
+
+
 
     }, this);
     this.input.on('gameobjectout', function(pointer, gameObject) {
@@ -286,6 +406,14 @@ class ChooseItemScene extends Phaser.Scene {
           player.DEF += (item.DEF-equip.armor.DEF);
           equip.armor = item;
           break;
+        case 'status':
+          player.HP += (item.HP);
+          player.ATK += (item.ATK);
+          player.DEF += (item.DEF);
+          shp += item.HP;
+          satk += item.ATK;
+          sdef += item.DEF;
+          break;
       }
       console.log("item: ", item);
       console.log("equip: ", equip);
@@ -301,11 +429,10 @@ class FightScene extends Phaser.Scene {
   }
   //get data from pre scene
   init() {
-    let level = 1;
     this.monster = {
-      HP : (level*3)+10,
-      ATK : (level*1)+4,
-      DEF : (level*1)+1
+      HP : (level*3)+10+(tier*2),
+      ATK : (level*1)+4+(tier*1),
+      DEF : (level*1)+1+(tier*1)
     }
     this.timer = 0;
     this.turn = 'p';
@@ -376,7 +503,7 @@ class FightScene extends Phaser.Scene {
     //fight event (per 1 second)
     this.playerHP = player.HP;
     this.monsterHP = this.monster.HP;
-    this.fightEvent = this.time.addEvent({ delay: 1000, callback: this.onEvent, callbackScope: this, loop: true });
+    this.fightEvent = this.time.addEvent({ delay: 500, callback: this.onEvent, callbackScope: this, loop: true });
 
   } //create end
 
@@ -395,7 +522,7 @@ class FightScene extends Phaser.Scene {
       this.monsterHP -= player.ATK;
       this.M__HP.setText('M_HP: ' + this.monsterHP).setTintFill(0xff0000);
       if (this.monsterHP<=0) {
-        this.scene.start('second');
+        this.stageClear(true);
         this.fightEvent.remove(false);
       }
       this.turn = 'm';
@@ -403,20 +530,74 @@ class FightScene extends Phaser.Scene {
       this.playerHP -= this.monster.ATK;
       this.HP.setText('HP: ' + this.playerHP).setTintFill(0xff0000);
       if (this.playerHP<=0 && this.monsterHP>0) {
-        this.scene.start('first');
+        this.stageClear(false);
         this.fightEvent.remove(false);
       }
       this.turn = 'p';
     }
-    console.log('fighting!', this.playerHP, this.monsterHP);
-
   }
+
+  //show ui when stage clear or fail
+  stageClear(clear) {
+    if (clear) {
+      console.log('Win!');
+      this.add.image(400, 200, 'win').setDisplaySize(400,300);
+
+      this.add.image(400, 400, 'button').setDisplaySize(200,100).setInteractive();
+      this.add.text(340, 385, '다음', { fontSize: 30, fontStyle: 'bold' });
+
+      this.input.on('gameobjectover', function(pointer, button) {
+        button.setTint(0xf0f0f0);
+      });
+      this.input.on('gameobjectout', function(pointer, button) {
+        button.clearTint();
+      });
+      this.input.on('gameobjectdown', function(pointer, button) {
+        level++;
+        this.scene.start('second');
+      }, this);
+
+    } else {
+      console.log('Fail...');
+      this.add.image(400, 200, 'lose').setDisplaySize(400,300);
+
+      let btnReplay = this.add.image(300, 400, 'button').setDisplaySize(200,100).setInteractive();
+      this.add.text(240, 385, '다시시작', { fontSize: 30, fontStyle: 'bold' });
+      let btnExit = this.add.image(500, 400, 'button').setDisplaySize(200,100).setInteractive();;
+      this.add.text(440, 385, '게임종료', { fontSize: 30, fontStyle: 'bold' });
+      btnReplay.name = 'replay';
+      btnExit.name = 'exit';
+
+      this.input.on('gameobjectover', function(pointer, button) {
+        button.setTint(0xf0f0f0);
+      });
+      this.input.on('gameobjectout', function(pointer, button) {
+        button.clearTint();
+      });
+      this.input.on('gameobjectdown', function(pointer, button) {
+        player.init();
+        equip.init();
+        level = 1;
+        tier = 1;
+        switch (button.name) {
+          case 'replay':
+            this.scene.start('first');
+            break;
+          case 'exit':
+            this.scene.start('intro');
+            break;
+        }
+      }, this);
+
+    }
+  }
+
 }
 
 let config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: gameWidth,
+  height: gameHeight,
   backgroundColor: '#fff',
   physics: {
     default: 'arcade',
